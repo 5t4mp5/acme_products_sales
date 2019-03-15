a23917ce0f17389188245686d3b3f987368f0c95
Dave Siegel <dave@Admins-MacBook-Pro.local>
2019-03-15 16:29:54 -0400

    api route works

---
+++ server/api/router.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { Product } = require("../db");
+
+router.get("/products", (req, res, next) => {
+    Product.findAll()
+        .then(products => res.json(products))
+        .catch(next);
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
 const { dbSyncAndSeed } = require("./db");
+const app = require("./app");
 
-dbSyncAndSeed().catch(e => console.error(e.message));
+const PORT = process.env.port || 3000;
+
+dbSyncAndSeed()
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`I'm Listening... on port ${PORT}`);
+    })
+  )
+  .catch(e => console.error(e.message));
