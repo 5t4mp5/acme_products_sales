ba70e65cf2eea4684b47b52e157dbd65f2257693
Dave Siegel <dave@Admins-MacBook-Pro.local>
2019-03-15 16:42:11 -0400

    wrote scratch routes for post and delete

--- server/api/router.js
+++ server/api/router.js
@@ -2,9 +2,29 @@ const router = require("express").Router();
 const { Product } = require("../db");
 
 router.get("/products", (req, res, next) => {
-    Product.findAll()
-        .then(products => res.json(products))
-        .catch(next);
+  Product.findAll()
+    .then(products => res.json(products))
+    .catch(next);
+});
+
+router.post("/products", (req, res, next) => {
+  const { name, price, discountPercentage, availability } = req.body;
+  const onSale = discountPercentage !== undefined;
+  Product.create({
+    name,
+    price,
+    discountPercentage,
+    availability,
+    onSale
+  })
+  .then(() => res.sendStatus(201))
+  .catch(next);
+});
+
+router.delete("/products", (req, res, next) => {
+  Product.destroy({ where: { id: req.body.id } })
+  .then(() => res.sendStatus(204))
+  .catch(next);
 });
 
 module.exports = router;
